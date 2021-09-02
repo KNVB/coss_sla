c461e373c27ed6407f6a57f2758135061356e9ca
SITO3 <bb@cc.com>
2021-09-02 11:29:22 +0800

    Modified the Fetch utility.

--- src/utility/Utility.js
+++ src/utility/Utility.js
@@ -1,58 +1,62 @@
 import SessionExpiredError from './SessionExpiredError';
-export default class Utility{
-  static async fetchAPI(url,method,params){
+export default class Utility {
+  static async fetchAPI(url, method, params) {
     console.log("=======================");
-    console.log("url="+url);
-    console.log("method="+method);
-    console.log("params="+params);
+    console.log("url=" + url);
+    console.log("method=" + method);
+    console.log("params=" + params);
     console.log("=======================");
-    let requestPara={
-      headers:{
+    let requestPara = {
+      headers: {
         'Content-Type': 'application/json'
       },
-      "method":method || 'GET',
+      "method": method || 'GET',
     }
-    switch (method.toUpperCase()){
+    switch (method.toUpperCase()) {
       case 'GET':
         const paramsObject = new URLSearchParams(params);
-        const queryString = paramsObject.toString();  
-        url+="?"+queryString;
+        const queryString = paramsObject.toString();
+        url += "?" + queryString;
         break;
       case 'POST':
-        requestPara["body"]=JSON.stringify(params);
+        requestPara["body"] = JSON.stringify(params);
         break;
       default:
-        break;  
+        break;
     }
-    let response = await fetch(url,requestPara);
-    /*
-    response.headers.forEach((value,name)=>{
-      console.log(name+","+value);
-    })
-    */
-    
-    if (response.headers.get('content-disposition')){
-      let value=response.headers.get('content-disposition');
-      if (value.indexOf("attachment; filename=")>-1){
-        let fileName=value.replace("attachment; filename=","").replaceAll("\"","");
-        let blob=await response.blob();
-        const newBlob = new Blob([blob], { type:response.headers.get('content-type')});
-        const objUrl = window.URL.createObjectURL(newBlob);
-        let link = document.createElement('a');
-        link.href = objUrl;
-        link.download = fileName;
-        link.click();
+    return fetch(url, requestPara)
+      .then(response => this.processResponse(response))
+      .catch(err => { throw new Error(err.message) });
+  }
+  static async processResponse(response){
+    if (response.ok){
+      let contentType=response.headers.get('content-type');
+      switch (contentType){
+        case "application/json; charset=utf-8":
+          return await response.json();
+          break;
+        case "text/html; charset=utf-8":
+          return await response.text();							
+          break;
+        default:
+          let value=response.headers.get('content-disposition');
+          let fileName=value.replace("attachment; filename=","").replaceAll("\"","");
+          let blob=await response.blob();
+          const newBlob = new Blob([blob], { type:response.headers.get('content-type')});
+          const objUrl = window.URL.createObjectURL(newBlob);
+          let link = document.createElement('a');
+          link.href = objUrl;
+          link.download = fileName;
+          link.click();
+          break;
       }
     } else {
       let responseObj=await response.json();
-      switch (response.status){
-        case 401:
-          throw new SessionExpiredError(responseObj.message);
-        case 200:
-          return responseObj;         
-        default:
-          throw new Error(responseObj.message);
+      if (response.status === 401 ){
+        throw new SessionExpiredError(responseObj.message);
+      } else {
+        throw new Error(responseObj.message);
       }
     }
-  }   
+  }
 }
